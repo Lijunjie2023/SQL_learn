a4ab01f2eb57a3b9b68c7a0eb4794ec6ccc848ad
Lijunjie2023 <ljj1352022@163.com>
2026-03-09 23:12:23 +0800

    本地修改

--- 13_SQL必知必会_创建高级联结.sql
+++ 13_SQL必知必会_创建高级联结.sql
@@ -7,7 +7,9 @@ USE sql_learn;
 # 允许在一条SELECT语句中多次使用相同的表。
 
 SELECT cust_name, cust_contact
-FROM customers, orders, orderitems
+FROM customers,
+     orders,
+     orderitems
 WHERE customers.cust_id = orders.cust_id
   AND orderitems.order_num = orders.order_num
   AND prod_id = 'RGAN01';
@@ -15,7 +17,9 @@ WHERE customers.cust_id = orders.cust_id
 # 使用表别名
 # 查询产品名称为"RGA01"的顾客名称、顾客联系人
 SELECT cust_name, cust_contact
-FROM customers AS C, orders AS O, orderitems AS OI
+FROM customers AS C,
+     orders AS O,
+     orderitems AS OI
 WHERE C.cust_id = O.cust_id
   AND OI.order_num = O.order_num
   AND prod_id = 'RGAN01';
@@ -28,14 +32,16 @@ WHERE C.cust_id = O.cust_id
 # 1、自联结，一般来说，自联结的效率会更高
 # 查询customers表中顾客联系方式JIM 的顾客名称
 SELECT c1.cust_id, c1.cust_name, c1.cust_contact
-FROM Customers AS c1, Customers AS c2
+FROM Customers AS c1,
+     Customers AS c2
 WHERE c1.cust_name = c2.cust_name
   AND c2.cust_contact = 'Jim Jones';
 
 # 先生成笛卡尔积
 # 再筛选Where两个条件
 SELECT c1.cust_id, c1.cust_name, c1.cust_contact, c2.cust_name, c2.cust_contact
-FROM Customers AS c1, Customers AS c2
+FROM Customers AS c1,
+     Customers AS c2
 WHERE c2.cust_contact = 'Jim Jones';
 
 
@@ -43,10 +49,24 @@ WHERE c2.cust_contact = 'Jim Jones';
 # 它会自动将两个表中所有同名且数据类型兼容的列作为连接条件，进行等值连接，并在结果中只保留一份这些同名列。
 # 事实上，我们迄今为止建立的每个内联结都是自然联结，很可能永远都不会用到不是自然联结的内联结。
 SELECT c.*, o.order_num, o.order_date, OI.prod_id, OI.quantity, OI.item_price
-FROM customers AS C, orders AS O, orderitems AS OI
+FROM customers AS C,
+     orders AS O,
+     orderitems AS OI
 WHERE c.cust_id = o.cust_id
   AND OI.order_num = o.order_num
   AND prod_id = 'RGAN01';
 
 
 # 外联结
+# 外联结可以查询到一些空状态
+# 检索所有顾客及其订单
+# 内联结
+SELECT customers.cust_id, orders.order_num
+FROM customers,
+     orders
+WHERE customers.cust_id = orders.cust_id;
+
+# 外联结 ,可以检索出没有订单的用户
+SELECT customers.cust_id, orders.order_num
+FROM customers
+LEFT OUTER JOIN orders ON customers.cust_id = orders.cust_id;
